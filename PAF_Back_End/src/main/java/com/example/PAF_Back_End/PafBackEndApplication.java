3be0d6d86d61312cff42f42967f5d097f6037bc9
Nadil17 <gnadil44@gmail.com>
2025-04-26 17:04:50 +0530

    commit files

---
+++ PAF_Back_End/src/main/java/com/example/PAF_Back_End/PafBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.PAF_Back_End;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PafBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PafBackEndApplication.class, args);
+	}
+
+}
