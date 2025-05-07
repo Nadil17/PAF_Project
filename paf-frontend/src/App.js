682840f12457693ed5a73b46b75036bd5398c7db
Dinidu823 <dinidu860@gmail.com>
2025-05-07 08:59:34 +0530

    updates to App.js for Q&A

--- paf-frontend/src/App.js
+++ paf-frontend/src/App.js
@@ -3,6 +3,11 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
 import Dashboard from './components/Dashboard';
 import './App.css';
+import QuestionList from './components/QuestionList';
+import QuestionDetail from './components/QuestionDetail';
+import AskQuestion from './components/AskQuestion';
+import EditQuestion from './components/EditQuestion';
+import EditAnswer from './components/EditAnswer';
 // import Posts from './components/Posts';
 // import CommentForm from './components/CommentForm';
 // import CommentSection from './components/CommentSection';
@@ -14,6 +19,11 @@ function App() {
         <Routes>
           <Route path="/" element={<LoginPage />} />
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/questions" element={<QuestionList/>} />
+          <Route path="/questions/:id" element={<QuestionDetail/>} />
+          <Route path="/ask-question" element={<AskQuestion/>} />
+          <Route path="/edit-question/:id" element={<EditQuestion />} />
+          <Route path="/edit-answer/:id" element={<EditAnswer />} />
           {/* <Route path="/createpost" element={<Posts />} />
           <Route path="/posts/:postId" element={<Posts />} />
           <Route path="/comment" element={<CommentForm />} />
